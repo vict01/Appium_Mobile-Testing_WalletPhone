7a6b6a7397f0fea555a65dd6c3f0328e47b2f902
VictorCaminero <caminerotorres@gmail.com>
2020-01-20 19:42:29 -0400

    Code optimization

--- README.md
+++ README.md
@@ -8,23 +8,20 @@ Record a income, record a expense, then evaluate if income minus expense are equ
 
 ### How to use the test suite
 ```
-1. Open MonefyMainClass java class
-2. Below the main method, there are two commented line, each of them with a String variable initialized with any values,
-   which corresponds to the income and expense amount (incomeAmount and expenseAmount, correspondingly) required to start testing.  
-3. Change the values of variables incomeAmount and expenseAmount as desired.
-4. Run MonefyMainClass java class
+1. Run MonefyMainClass java class
+2. When appear the Income Amount request, please enter this value
+3. When appear the Expense Amount request, please enter this value
+4. Wait for testing finishes while this running. 
 5. Once test finish to run,in the IDE console you'll see a printed message indicating if the test succeeded or failed, 
    based on the scenario definition in above section. 
 ```
 
 ### Folder structure
 - lib folder (stored in the project root folder): Which contains all libraries used in the project. 
-- MonefyBaseClass java class: Which contains all methods used in MonefyMainClass java class.
-- MonefyMainClass java class: Which contains the main method, and all steps that make up the test. 
+- GenericMethods java class: Which contains generic methods used in MonefyTest java class.
+- MonefyTest java class: Which contains the main method, and all steps that make up the test. 
+- screens Package: Which contains one class for each screen used in MonefyTest java class.
 - com.monefy.app.lite_2019-10-27.apk: APK file, based on which run the automated test.
-
-### Note
-In general terms the best practice to build the framework structure would be create one java class for each phone screen (with its corresponding attributes and methods) and then instanciate it from main method using Page Object Model as needed; however considering I had time only for automate one scenario using just a few screen and buttons where most element had the same locator, I considered this pattern a risk to finish ontime and probably an unnecessary effort in this specific situation. 
 
 ### Used tools/technologies
 ```
--- src/N26/monefy/GenericMethods.java
+++ src/N26/monefy/GenericMethods.java
@@ -1,18 +1,19 @@
-package N26.company;
+package N26.monefy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.io.File;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
-import java.net.MalformedURLException;
-import java.util.List;
 import java.math.BigDecimal;
 
-public class MonefyBaseClass {
+public class GenericMethods {
 
-    public static  AndroidDriver<AndroidElement> capabilities() throws Exception {
+    public static AndroidDriver<AndroidElement> capabilities() throws Exception {
         File apkPath = new File("src");
         File apkName = new File(apkPath, "com.monefy.app.lite_2019-10-27.apk");
         DesiredCapabilities cap = new DesiredCapabilities();
@@ -21,8 +22,16 @@ public class MonefyBaseClass {
         cap.setCapability(MobileCapabilityType.APP, apkName.getAbsolutePath());
         cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
 
-        AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+        AndroidDriver<AndroidElement> driver;
+        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
         return driver;
+    }
+
+    public static void printMsg(String value) { System.out.println(value); }
+
+    public static String readValue () throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return reader.readLine();
     }
 
     public static void typeAmountId(String value, AndroidDriver<AndroidElement> driver) {
@@ -40,25 +49,15 @@ public class MonefyBaseClass {
         }
     }
 
-    public static List<AndroidElement> typeElementId(AndroidDriver<AndroidElement> driver, String str){
-        final List<AndroidElement> amounts = driver.findElementsById(str);
-        return amounts;
-    }
-
-    public static BigDecimal restTwoNum(BigDecimal valueA, BigDecimal valueB) {
-        BigDecimal result = new BigDecimal(String.valueOf(valueA)).subtract(valueB);
-       return result;
-    }
-
     public static BigDecimal str2BigDecimal (String num){
-        BigDecimal newValue = new BigDecimal(num).setScale(2, BigDecimal.ROUND_DOWN);
+        BigDecimal newValue;
+        newValue = new BigDecimal(num).setScale(2, BigDecimal.ROUND_DOWN);
         return newValue;
     }
 
     public static String removeChar(String str, String charValue) {
         int aLength = charValue.length();
         int begging = 0;
-
         for (int i=1; i<=aLength; i++){
             String aSubstring = charValue.substring(begging, i);
             str = str.replace(aSubstring, "");
@@ -71,9 +70,13 @@ public class MonefyBaseClass {
         return firstNumber.equals(secondNumber);
     }
 
-    public static void printElement(String value) {
-        System.out.println(value);
+    public static boolean isNumeric(String str){
+        try {
+            BigDecimal newValue = new BigDecimal(str).setScale(2, BigDecimal.ROUND_DOWN);
+            return true;
+        }catch (NumberFormatException e){
+            return false;
+        }
     }
-
 
 }
